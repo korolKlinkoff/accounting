21e66a1511a7ea934f159cc7a26d298bbe472cad
Cosmos <korol.klinkof@gmail.com>
2024-11-12 15:30:38 +0300

    added cool progressbar

--- application/db/people.py
+++ application/db/people.py
@@ -1,2 +1,9 @@
+from tqdm import tqdm
+from time import sleep
+
+
 def get_employees():
+    # cool progressbar for "db"
+    for i in tqdm(range(10)):
+        sleep(0.1)
     return ["obj1", "obj2", "obj3"]
